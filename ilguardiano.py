194b510c4d363482d7297c473bec4981c4b6242d
L0cutus <mie.iscrizioni@gmail.com>
2009-06-12 13:15:07 +0200

    eliminati msg ed aggiunte le basi per il ban
    ho integrato i messaggi per i sproloquiatori nel comando kick,
    ho integrato nella variabile killed vari dati per il successivo
    processing per il ban a seconda di quante volte l'user si e' fatto
    kickare.

--- ilguardiano.py
+++ ilguardiano.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-#
+# -*- coding: utf-8 -*-
 
 __module_name__ = "IlGuardiano"
-__module_version__ = "1.01"
+__module_version__ = "0.2.0"
 __module_description__ = "Modulo Python per kickare chi dice parole sgradevoli"
 __module_author__ = "Stefano Zamprogno <mie.iscrizioni@gmail.com>"
 
@@ -11,9 +11,11 @@ import shelve
 import os
 import time
 import sys
+import random
+import string
 
-weechat.register("ilguardiano.py", "0.2.0", "", "Gestione Bestemmie v.0.2.0", "UTF8")
-
+weechat.register("ilguardiano.py", "0.2.0", "",
+                            "Gestione Bestemmie v.0.2.0", "UTF8")
 # definizione dati di default
 parolacce = {'dio porco':'Contegno! mi consenta...',
              'dioporco':'Contegno! mi consenta...',
@@ -43,13 +45,23 @@ parolacce = {'dio porco':'Contegno! mi consenta...',
             }
 
 # network/canali da monitorare
-chans = {'azzurra':set(['#archlinux','#nokia','#ubuntu','#tst']),
+chans = {'azzurra':set(['#archlinux','#nokia','#ubuntu','#energiasolare']),
          'freenode':set(['#archlinux.it',])}
 
 # devo killare anche gli op?
 killop = False
+# formato di killed
+# {(tupla:nick,chan,network):[lista:(tupla:hour,min,sec)*n]}
+# es.
+#{('AserTo', '#nokia', 'azzurra'): [(7, 45, 26), (7, 45, 28)],
+# ('L0cutus', '#nokia', 'azzurra'): [(7, 45, 26), (7, 45, 28), (7, 45, 35)],
+# ('kk', '#archlinux', 'azzurra'): [(7, 45, 26), (7, 45, 28), (7, 45, 35)]}
+killed = {}
 mynick = 'L0cutus'
 kick_queue = set()
+op = False
+tchan = None
+tsrv = None
 
 # -----------------------------------------------------------
 # Non modificare oltre questa linea--------------------------
@@ -57,35 +69,42 @@ kick_queue = set()
 
 db = shelve.open('.ilguardiano.db')
 
-if db.has_key('chans'):
+if 'chans' in db:
     # su che canali e' attivo il guardiano
     chans = db['chans']
 
-if db.has_key('parolacce'):
+if 'parolacce' in db:
     parolacce = db['parolacce']
 
-if db.has_key('killop'):
+if 'killop' in db:
     killop = db['killop']
 
 def hook_mode_cb(server, args):
+    global op,mynick
+    if '+o' not in args:
+        return weechat.PLUGIN_RC_OK
+    res = args.split(" ")
+    weechat.prnt("Len args: %s" % len(res), '#tst', 'azzurra')
+    weechat.prnt("Res: %s" % res, '#tst', 'azzurra')
     null, null, chan, mode, nick = args.split(" ", 5)
-    if (chan in chans[server]):
+    weechat.prnt("nick: %s\nchan; %s" % (nick,chan), '#tst', 'azzurra')
+    if (server in chans and chan in chans[server]):
         if nick == mynick:
-            if mode == '+o':
-                if kick_queue:
-                    for cmd in kick_queue:
-                        weechat.command("/kick %s" % cmd[0], cmd[1], cmd[2])
-                        kick_queue.discard([cmd[0], cmd[1], cmd[2]])
-                    weechat.command("/cs deop %s %s" % (cmd[1], mynick), cmd[1], cmd[2])
+            if not op and mode == '+o':
+                op = True
+                weechat.add_timer_handler(20, "hook_timer_cb") # 20sec di op
+            elif mode == '-o':
+                op = False
     return weechat.PLUGIN_RC_OK
 
 
 def hook_msg_cb(server, args):
+    global op,tchan,tsrv,mynick
     null, chan, testo = args.split(":", 2)
     mask, null, chan = chan.strip().split(" ", 2)
     nick, mask = mask.split("!", 1)
     network = server
-    testo = testo.lower()
+    testo = testo.lower().strip()
     isop = False
     # se non si possono killare gli op
     # occorre verificare se l'user e' un op
@@ -108,16 +127,36 @@ def hook_msg_cb(server, args):
     if not (isop and (not killop)):
         # se il network e' tra quelli osservati e il canale e'
         # tra quelli monitorati...
-        if (network in chans) and (chan in chans[network]):
+        if (network in chans and chan in chans[network]):
             for p in parolacce.keys():
                 if p in testo:
-                    weechat.command("/msg %s %s,%s" % (chan,
-                                    nick, parolacce[p]), chan, network)
-                    kick_queue.add((nick,chan,network))
-                    weechat.command("/cs op %s L0cutus" % (chan), '', network)
+                    kick_queue.add((nick,chan,network,parolacce[p]))
+                    if not op:
+                        tchan = chan
+                        tsrv = network
+                        weechat.command("/cs op %s %s" % (chan, mynick), '', network)
                     break
     return weechat.PLUGIN_RC_OK
 
+def hook_tmr_queue_cb():
+    global op,kick_queue,killed
+    if kick_queue:
+        for cmd in list(kick_queue):
+            if op:
+                weechat.command("/kick %s %s" % (cmd[0], cmd[3]), cmd[1], cmd[2])
+                if (cmd[0],cmd[1],cmd[2]) not in killed:
+                    killed[(cmd[0],cmd[1],cmd[2])] = [(time.time()),]
+                else:
+                    killed[(cmd[0],cmd[1],cmd[2])].append((time.time()))
+                kick_queue.discard((cmd[0], cmd[1], cmd[2], cmd[3]))
+    return weechat.PLUGIN_RC_OK
+
+def hook_timer_cb():
+    global op,tchan,tsrv,mynick
+    op = False
+    weechat.command("/cs deop %s %s" % (tchan, mynick), '', tsrv)
+    weechat.remove_timer_handler('hook_timer_cb')
+    return weechat.PLUGIN_RC_OK
 
 # aggiunge una nuova parolaccia
 def hook_addword_cb(server, args):
@@ -147,7 +186,7 @@ def hook_addchan_cb(server, args):
     else:
         netw = dati[0].lower()
         chns = dati[1].lower()
-        if not chans.has_key(netw):
+        if netw not in chans:
             chans[netw]=set()
 
         chns = list(chns.split(','))
@@ -163,7 +202,7 @@ def hook_delchan_cb(server, args):
     dati = list(args.split(' ', 1))
     if len(dati) == 1: # cancella tutto il network
         # c'e' il network ?
-        if chans.has_key(dati[0].lower()):
+        if dati[0].lower() in chans:
             del chans[dati[0].lower()]
             db['chans'] = chans
             weechat.prnt("Cancellato server: %s" % dati[0])
@@ -173,7 +212,7 @@ def hook_delchan_cb(server, args):
         netw = dati[0].lower()  # network
         chns = list(dati[1].lower().split(','))  # canali
         # c'e' il network ?
-        if chans.has_key(netw):
+        if netw in chans:
             # esiste il canale da cancellare ?
             for i in chns:
                 chans[netw].discard(i)
@@ -198,6 +237,15 @@ def hook_pripar_cb(server, args):
     weechat.prnt("Parolacce:")
     for i in parolacce:
         weechat.prnt("%s : %s" % (i, parolacce[i]))
+    return weechat.PLUGIN_RC_OK_IGNORE_ALL
+
+# stampa la lista dei killati
+def hook_prikilled_cb(server, args):
+    weechat.prnt("Lista killati:")
+    for i in killed.keys():
+        weechat.prnt("Nick: %s, Canale: %s, Server: %s" % (i[0], i[1], i[2]))
+        for t in killed[i]:
+            weechat.prnt("Time: %s" % (time.localtime(t)))
     return weechat.PLUGIN_RC_OK_IGNORE_ALL
 
 # stampa l'intero archivio canali
@@ -231,6 +279,7 @@ def hook_prihelp_cb(server, args):
         weechat.command(i)
     return weechat.PLUGIN_RC_OK_IGNORE_ALL
 
+weechat.add_timer_handler(2, "hook_tmr_queue_cb")
 
 weechat.add_message_handler("privmsg",
                             "hook_msg_cb")
@@ -267,5 +316,8 @@ weechat.add_command_handler("delchan", "hook_delchan_cb", "",
 
 weechat.add_command_handler("delword", "hook_delword_cb", "",
                 "bestemmia : Cancella una bestemmia dalla lista")
+
+weechat.add_command_handler("prikilled", "hook_prikilled_cb", "",
+                ": mostra i vari nick che sono stati killati")
 
 weechat.prnt("Plugin IlGuardiano loaded!")
